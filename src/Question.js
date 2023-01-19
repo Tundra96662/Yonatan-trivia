ea5a801f9ac364368032760200e5ce65f88007b3
Tundra <73200419+Tundra96662@users.noreply.github.com>
2023-01-19 11:35:21 -0600

    initial commit

--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,10 @@
-function Question() {
-  return <div></div>;
+function Question(props) {
+  return (
+    <div>
+      <h1>{props.text}</h1>
+      <h1> </h1>
+    </div>
+  );
 }
 
 export default Question;
